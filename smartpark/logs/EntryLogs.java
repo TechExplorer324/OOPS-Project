936235abe640adfdca08211f457f833090c69ea5
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-06 18:53:51 +0530

    Update EntryLogs.java

--- smartpark/logs/EntryLogs.java
+++ smartpark/logs/EntryLogs.java
@@ -3,11 +3,11 @@ import java.time.*;
 class EntryLogs{
   LocalDate localDate = LocalDate.now();
   LocalTime localTime = LocalTime.now();
-  boolean success;
+  boolean success1;
 
 EntryLogs(localDate,localTime,success){
   this.localDate=localDate;
   this.localTime=localTime;
-  this.success=success;  
+  this.success1=success1;  
 }
 }
