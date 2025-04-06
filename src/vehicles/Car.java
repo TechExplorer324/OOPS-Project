bfaea565a71a4da653bfc6f2cc27641293459286
TechExplorer324 <146563720+TechExplorer324@users.noreply.github.com>
2025-04-06 17:33:26 +0530

    Update Car.java

--- src/vehicles/Car.java
+++ src/vehicles/Car.java
@@ -34,5 +34,59 @@ public class Car extends Vehicle {
     }
 }
 
+public class Car extends Vehicles{
+
+    String carCompany;
+    String carType;
+    int noOfPassengers;
+    
+
+
+    public Car(String licensePlate, VehicleType type){
+        super(licensePlate, type);
+        }
+
+   public Vehicle(String licensePlate, VehicleType type, String ownerName) {
+         super(licensePlate, type, ownerName);
+    }
+
+    public Vehicle(String licensePlate, VehicleType type, String ownerName, String color) {
+       super(licensePlate, type, ownerName, color);
+
+        }
+
+    public String getCarCompany(){
+        return CarCompany;
+    }
+
+     public String getCarType(){
+        return CarType;
+    }
+
+    public String getNoOfPassengers(){
+        return noOfPassengers;
+    }
+
+    public void setCarCompany(String carCompany){
+        this.carCompany = carCompany;
+    }
+
+    public void setNoOfPassengers(int Passengers){
+        this.noOfPassengers = Passengers;
+    }
+
+    public void setCarType(String carType){
+        this.carType = carType;
+    }
+
+
+    
+
+
+
+    
+}
+
+
     
 // }
