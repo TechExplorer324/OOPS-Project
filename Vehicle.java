5b81b12fbaf2ca3bd4456c20c797312575d8ddb1
TechExplorer324 <146563720+TechExplorer324@users.noreply.github.com>
2025-04-06 16:04:56 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,1 +1,22 @@
+public class Vehicles{
 
+    private String Type;
+    private String VehicleNo;
+
+    public Vehicles(String Type, String VehicleNo){
+        this.Type = Type;
+        this.VehicleNo = VehicleNo;
+    }
+
+    public String getType(){
+        return Type;
+    }
+
+    public String getVehicleNo(){
+        return VehicleNo;
+    }
+
+
+
+    
+}
