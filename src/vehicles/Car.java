46f14e26f11fb03ea4cc5c5f04caea40ee1d25e1
adityapra1310 <adityaprakash1310@gmail.com>
2025-04-06 17:21:11 +0530

    Update Car.java
    updated car.java

--- src/vehicles/Car.java
+++ src/vehicles/Car.java
@@ -1,26 +1,38 @@
-public class Car extends Vehicles{
+// public class Car extends Vehicles{
 
-    String color;
-    String CarCompany;
+//     String color;
+//     String CarCompany;
 
 
 
-    public Car(String Type, String VehicleNo, String color, String carCompany){
-        super(Type, VehicleNo);
-        this.color = color;
-        this.carCompany = carCompany;
+//     public Car(String Type, String VehicleNo, String color, String carCompany){
+//         super(Type, VehicleNo);
+//         this.color = color;
+//         this.carCompany = carCompany;
 
+//     }
+
+//     public String getColor(){
+//         return color;
+//     }
+
+//     public String getCarCompany(){
+//         return carCompany;
+//     }
+
+public class Car extends Vehicle {
+    public Car(String licensePlate) {
+        super(licensePlate, VehicleType.CAR);
+    }
+     public Car(String licensePlate, String ownerName) {
+        super(licensePlate, VehicleType.CAR, ownerName);
     }
 
-    public String getColor(){
-        return color;
+    @Override
+    public double getRequiredSpotSize() {
+        return 1.0; // Standard car size unit
     }
-
-    public String getCarCompany(){
-        return carCompany;
-    }
-
-
+}
 
     
-}
+// }
