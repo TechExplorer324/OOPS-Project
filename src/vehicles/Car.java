ff6d3dabb67272e4471a44176dc33e7817868a81
Shashwat <f20212824@goa.bits-pilani.ac.in>
2025-04-06 18:12:57 +0530

    CarType.java created and done

--- src/vehicles/Car.java
+++ src/vehicles/Car.java
@@ -1,94 +1,52 @@
-// public class Car extends Vehicles{
+package vehicles;
 
-//     String color;
-//     String CarCompany;
-
-
-
-//     public Car(String Type, String VehicleNo, String color, String carCompany){
-//         super(Type, VehicleNo);
-//         this.color = color;
-//         this.carCompany = carCompany;
-
-//     }
-
-//     public String getColor(){
-//         return color;
-//     }
-
-//     public String getCarCompany(){
-//         return carCompany;
-//     }
-
-// }
-
-/*
 public class Car extends Vehicle {
-    public Car(String licensePlate) {
-        super(licensePlate, VehicleType.CAR);
-    }
-     public Car(String licensePlate, String ownerName) {
-        super(licensePlate, VehicleType.CAR, ownerName);
-    }
-
-    @Override
-    public double getRequiredSpotSize() {
-        return 1.0; // Standard car size unit
-    }
-}
-*/
-
-public class Car extends Vehicle{
 
     String carCompany;
     String carType;
     int noOfPassengers;
-    
 
 
-    public Car(String licensePlate, CarType type){
-        super(licensePlate, VehicleType.);
-        }
-
-   public Car(String licensePlate, VehicleType type, String ownerName) {
-         super(licensePlate, type, ownerName);
+    public Car (String licensePlate, String color, CarType type, String carCompany, String carType, int noOfPassengers) {
+        super(licensePlate, VehicleType.CAR, color);
+        this.carCompany = carCompany;
+        this.carType = carType;
+        this.noOfPassengers = noOfPassengers;
     }
 
-    public Car(String licensePlate, VehicleType type, String ownerName, String color) {
-       super(licensePlate, type, ownerName, color);
-
-        }
-
-    public String getCarCompany(){
-        return CarCompany;
+    public Car (String licensePlate, VehicleType type, String color, String ownerName, String carCompany, String carType, int noOfPassengers) {
+        super(licensePlate, VehicleType.CAR, color, ownerName);
+        this.carCompany = carCompany;
+        this.carType = carType;
+        this.noOfPassengers = noOfPassengers;
     }
 
-     public String getCarType(){
-        return CarType;
+
+    public String getCarCompany() {
+        return carCompany;
     }
 
-    public String getNoOfPassengers(){
+    public String getCarType() {
+        return carType;
+    }
+
+    public int getNoOfPassengers() {
         return noOfPassengers;
     }
 
-    public void setCarCompany(String carCompany){
+    public void setCarCompany(String carCompany) {
         this.carCompany = carCompany;
     }
 
-    public void setNoOfPassengers(int Passengers){
+    public void setNoOfPassengers(int Passengers) {
         this.noOfPassengers = Passengers;
     }
 
-    public void setCarType(String carType){
+    public void setCarType(String carType) {
         this.carType = carType;
     }
 
 
-    
-
-
-
-    
 }
 
 
