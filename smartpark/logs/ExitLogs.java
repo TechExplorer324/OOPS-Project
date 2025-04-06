a4c6aefc8bb2bab3254cdc4c81fb8b880df77a21
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-06 18:54:22 +0530

    Update ExitLogs.java

---
+++ smartpark/logs/ExitLogs.java
@@ -0,0 +1,12 @@
+import java.time.*;
+class ExitLogs{
+  LocalDate localDate = LocalDate.now();
+  LocalTime localTime = LocalTime.now();
+  boolean success2;
+
+ExitLogs(localDate,localTime,success2){
+  this.localDate=localDate;
+  this.localTime=localTime;
+  this.success2=success2;  
+}
+}
